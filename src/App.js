2eb26d0802ec2933bd405766614aed5f1352d8f6
conan <sarimiro@naver.com>
2023-08-10 14:50:58 +0900

    동적 데이터 렌더링

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
 
@@ -8,17 +9,7 @@ function App() {
 
   return (
     <>
-      <div className="App">
-        <h1>메롱메롱</h1>
-        { $h2 }
-      </div>
-      <div className='noname'>
-        <input type='text' />
-        <p>
-          오늘은 태풍이 와서 비가 많이 내렸다. <br />
-          그래서 공부가 하기 싫다.
-        </p>
-      </div>
+      <ExpenseItem />
     </>
   );
 }
