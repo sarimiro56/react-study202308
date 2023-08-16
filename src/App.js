3ddc2968bb0aa3008a47083bed1df1510ef6e57e
conan <sarimiro@naver.com>
2023-08-16 11:53:14 +0900

    조건부 렌더링

--- src/App.js
+++ src/App.js
@@ -1,47 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseList from './components/Expenses/ExpenseList';
 import NewExpense from './components/NewExpense/NewExpense';
 
 
 const App = () => {
+
   // 지출 항목 객체 배열
   const expenses = [
     {
-      id: 1, 
+      id: 1,
       title: '바나나',
       price: 2000,
       date: new Date(2023, 3 - 1, 23),
     },
     {
-      id: 2, 
+      id: 2,
       title: 'BBQ치킨',
       price: 20000,
       date: new Date(2022, 5 - 1, 21),
     },
     {
-      id: 3, 
+      id: 3,
       title: '도미노피자',
       price: 35000,
       date: new Date(2023, 7 - 1, 14),
     },
     {
-      id: 4, 
+      id: 4,
       title: '엽기떡볶이',
       price: 17000,
       date: new Date(2021, 3 - 1, 28),
-    }
+    },
   ];
+
+  // 지출 객체배열을 상태변수로 관리
+  const [expenseList, setExpenseList] = useState(expenses);
+
 
   // ExpenseForm에게 내려보낼 함수
   const addExpenseHandler = (newExpense) => {
-    console.log('App컴포넌트에서 응답함!');
     console.log(newExpense);
+    setExpenseList([...expenseList, newExpense]);
   };
 
   return (
     <>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <ExpenseList items={expenses} />
+      <ExpenseList items={expenseList} />
     </>
   );
 };
