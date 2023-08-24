5c74fbb7d9ac5fb978c5d78848e50c23970e6b66
conan <sarimiro@naver.com>
2023-08-24 10:51:10 +0900

    Cart Context와 Reducer 생성

--- src/components/Food/Layout/Header.js
+++ src/components/Food/Layout/Header.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import styles from './Header.module.scss';
-import mealsImage from '../../../assets/meals.jpg'
+import mealsImage from '../../../assets/meals.jpg';
 import HeaderCartButton from './HeaderCartButton';
 
-const Header = () => {
+const Header = ({onShowCart}) => {
 
-    const { header, 'main-image': mainImage } = styles;
-    return (
-        <>
-            <header className={header}>
-                <h1>ReactMeals</h1>
-                <HeaderCartButton />
-            </header>
-            <div className={mainImage}>
-                <img src={mealsImage} alt='Looks very delicious meals' />
-            </div>
-        </>
-    );
-};
+  const { header, 'main-image': mainImage } = styles;
 
-export default Header;
+  return (
+    <>
+      <header className={header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onShow={onShowCart} />
+      </header>
+      <div className={mainImage}>
+        <img src={mealsImage} alt='Looks very delicious meals' />
+      </div>
+    </>
+  )
+}
+
+export default Header
