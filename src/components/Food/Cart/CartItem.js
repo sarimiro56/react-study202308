b946bcb299952a0612ac02af30441402fca39128
conan <sarimiro@naver.com>
2023-08-25 10:28:22 +0900

    장바구니 화면에서 - 버튼 처리

--- src/components/Food/Cart/CartItem.js
+++ src/components/Food/Cart/CartItem.js
@@ -4,9 +4,10 @@ import styles from './CartItem.module.scss';
 import CartContext from '../../../store/cart-context';
 
 const CartItem = ({ cart }) => {
-  const { addItem } = useContext(CartContext);
 
-  const { name, price, amount } = cart;
+  const { addItem, removeItem } = useContext(CartContext);
+
+  const { id, name, price, amount } = cart;
 
   const {
     'cart-item': cartItem,
@@ -21,6 +22,10 @@ const CartItem = ({ cart }) => {
     addItem({...cart, amount: 1});
   };
 
+  const cartRemoveItemHandler = () => {
+    removeItem(id);
+  };
+
   return (
     <li className={cartItem}>
       <div>
@@ -31,7 +36,7 @@ const CartItem = ({ cart }) => {
         </div>
       </div>
       <div className={actions}>
-        <button>−</button>
+        <button onClick={cartRemoveItemHandler}>−</button>
         <button onClick={cartAddItemHandler}>+</button>
       </div>
     </li>
